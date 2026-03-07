d72d1ca24f6bcbd270f3b00fd4ccb7c1104b9bc9
Raul <raulramosneves9@gmail.com>
2026-03-07 14:41:11 -0300

    botão incrito criado

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const sideBar = document.getElementById('sidebar');
 const menuIcon = document.getElementById('menu-icon');
-const 
+const signUpButton = document.getElementById('btn-inscrever');
+
+let inscrito = false;
+
+// FUNÇÃO INSCRITO OU NÃO //
+if (localStorage.getItem('inscrito') === 'true') {
+    inscrito = true;
+    signUpButton.classList.add('inscrito');
+    signUpButton.textContent = 'Inscrito ✔';
+}
+
+signUpButton.addEventListener('click', function () {
+    if (inscrito === false) {
+        inscrito = true;
+        signUpButton.classList.add('inscrito');
+        signUpButton.textContent = 'Inscrito ✔';
+        localStorage.setItem('inscrito', 'true'); // salva
+    } else {
+        inscrito = false;
+        signUpButton.classList.remove('inscrito');
+        signUpButton.textContent = 'Inscrever-se';
+        localStorage.setItem('inscrito', 'false'); // salva
+    }
+});
